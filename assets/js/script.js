768decfdf1e85510c4ec00a7c8b24f720d06fd37
ZeuS <ponzeadrian@gmail.com>
2024-01-07 17:35:23 +1100

    targeted HTML elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,15 @@
 var weatherKey = "ba9a914966ee540c726610644ae08e20"
 var url;
 var forecastUrl;
+
+// HTML elements
+var searchForm = document.getElementById("searchForm");
+var cityInput = document.getElementById("cityInput");
+var cityNameEl = document.getElementById("cityName");
+var dateEl = document.getElementById("date");
+var weatherIconEl = document.getElementById("weatherIcon");
+var temperatureEl = document.getElementById("temperature");
+var humidityEl = document.getElementById("humidity");
+var windSpeedEl = document.getElementById("windSpeed");
+var forecastContainerEl = document.getElementById("forecastContainer");
+
